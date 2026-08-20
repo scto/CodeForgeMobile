33e254d6cf2f306c2cf565c2121b2975e5d86a9a
Thomas Schmid <1441369+scto@users.noreply.github.com>
2026-08-20 12:07:44 +0200

    Delete CodeForgeMobile directory

--- CodeForgeMobile/app/src/main/kotlin/com/codeforge/app/CodeForgeApplication.kt
+++
@@ -1,8 +0,0 @@
-// Modul: :app
-package com.codeforge.app
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class CodeForgeApplication : Application()
--- CodeForgeMobile/core/domain/src/main/kotlin/com/codeforge/core/domain/model/FileHandle.kt
+++
@@ -1,7 +0,0 @@
-// Modul: :core:domain
-package com.codeforge.core.domain.model
-
-data class FileHandle(
-    val path: String,
-    val content: String
-)
