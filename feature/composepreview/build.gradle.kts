9c17503332cbe9e9639577ff6b2344173cbe0faf
Thomas Schmid <1441369+scto@users.noreply.github.com>
2026-08-23 23:51:11 +0200

    Set Java compatibility to version 17

--- feature/composepreview/build.gradle.kts
+++ feature/composepreview/build.gradle.kts
@@ -18,6 +18,11 @@ android {
     buildFeatures {
         compose = true
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
 
 dependencies {
